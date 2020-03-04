060153a76bc1c5ca915ab4e4758183a1e32710e8
Giedrius <giedrius@blokas.io>
2020-03-04 22:59:36 +0000

    TouchOSC 2 MIDI start script and service files for MODEP

---
+++ start_touchosc2midi.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+PORT_ID=$(/usr/local/bin/touchosc2midi list ports 2>&1 | grep touchosc | head -n 1 | egrep -o "\s+[0-9]+: " | egrep -o "[0-9]+")
+exec /usr/bin/touchosc2midi --midi-in=$PORT_ID --midi-out=$PORT_ID
